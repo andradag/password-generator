17d9daf53828c19419f68ebe736809142eab8861
Andrada Gacichevici <andradagacichevici@gmail.com>
2021-09-30 08:54:07 +0100

    options for computer to pick from

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,14 @@ function generatePassword() {
   // PLEASE ADD ALL YOUR CODE HERE
 }
 
+// Array of options for computer to pick from
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialCharacters = ["!", """, "#", "$", "%", "&", "'", "(", ")", "\", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+
+ 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
