cb98bfadf00fa1cb7a0683443031154a722a476e
Andrada Gacichevici <andradagacichevici@gmail.com>
2021-10-05 17:46:00 +0100

    prompt choose password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,22 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // PLEASE ADD ALL YOUR CODE HERE
+
+  //prompt choose password length
+
+  const passwordLengthtext = prompt(
+    "Please choose the length of the password between 8 and 128"
+  );
+
+  console.log(passwordLength);
+  var passworthLength = parseInt(passwordLengthtext);
+
+  if (passworthLength >= 8 && passworthLengthtext < 128) {
+  } else {
+  }
+
+  let complexity = document.getElementById("slider").value;
 }
-
-// Array of options for computer to pick from
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialCharacters = ["!", """, "#", "$", "%", "&", "'", "(", ")", "\", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "`", "{", "|", "}", "~"]
-
-
- 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
